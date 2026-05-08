581fdc6c2248f07aba03e8d054a7bf950829de44
loickcherimont <loickcherimont@gmail.com>
2026-05-08 22:31:03 +0200

    build: setup debiting dev environment

---
+++ src/test/java/com/github/loickcherimont/debiting_springboot/DebitingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.loickcherimont.debiting_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
